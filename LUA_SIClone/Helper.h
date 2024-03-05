f561cf941610a19e22d5990766f22f3decea768f
HarrisonH4 <145985397+HarrisonH4@users.noreply.github.com>
2024-03-05 14:50:36 +0000

    Started tutorial 2

--- LUA_SIClone/Helper.h
+++ LUA_SIClone/Helper.h
@@ -14,3 +14,8 @@ extern "C"
 	#include "../lua-5.4.4/include/lauxlib.h"
 	#include "../lua-5.4.4/include/lualib.h"
 }
+
+bool LuaOK(lua_State* L, int id);
+int LuaGetInt(lua_State* L, const std::string& name);
+std::string LuaGetStr(lua_State* L, const std::string& name);
+
---
+++ LUA_SIClone/LuaScript.lua
@@ -0,0 +1,7 @@
+--
+
+colour = 100
+level = 25
+lives = 10
+playerSprite = "assets/ufoph.bmp"
+
