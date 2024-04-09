09bf4e9e7aba10b574085ec197bca20e81749515
HarrisonH4 <145985397+HarrisonH4@users.noreply.github.com>
2024-04-09 13:49:31 +0100

    Adding C++ Lua Script

--- LUA_SIClone/Helper.cpp
+++ LUA_SIClone/Helper.cpp
@@ -81,3 +81,23 @@ void CallVoidVoidCFunc(lua_State* L, const std::string& fname)
 	if (!LuaOK(L, lua_pcall(L, 0, 0, 0)))
 		assert(false);
 }
+
+
+std::map <string, Dispatcher::Command> Dispatcher::library;
+
+int Dispatcher::LuaCall(lua_State* L)
+{
+	string name = lua_tostring(L, 1);
+	std::map<string, Command>::iterator it = library.find(name);
+	assert(it != library.end());
+	Command& cmd = (*it).second;
+	if (cmd.voidintfunc)
+	{
+		int param = lua_tointeger(L, 2);
+		cmd.voidintfunc(param);
+		lua_pop(L, 1);
+	}
+	else
+		assert(false);
+	return 1;
+}
